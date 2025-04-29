8eb0e87f5fa0cbb6363c0b94733d62cec96f3315
morev <n.morev@studyworld.io>
2025-04-29 17:50:06 +0300

    node-6 |  basic app server

---
+++ node/src/apps/best-course-ever/config.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '..', '.env') })
+
+export const config = {
+    appPort: process.env.BEST_COURSE_EVER_PORT || 4444,
+}
