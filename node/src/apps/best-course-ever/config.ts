73c9a059275b8e5152abf9c06348d18c01778e58
morev <n.morev@studyworld.io>
2025-05-14 15:49:06 +0300

    node-8 |  added setup to run mongodb

--- node/src/apps/best-course-ever/config.ts
+++ node/src/apps/best-course-ever/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({ path: path.resolve(__dirname, '..', '..', '..', '.env') })
+dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 export const config = {
     appPort: process.env.BEST_COURSE_EVER_PORT || 4444
